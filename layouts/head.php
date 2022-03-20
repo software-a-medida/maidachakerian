4123169658b894eadc2efcd6345f8b1258fc2bcd
Gersón Gómez <gergomez18@gmail.com>
2022-03-20 18:07:51 -0500

    Actualización de íconos en el head.

--- layouts/head.php
+++ layouts/head.php
@@ -21,17 +21,18 @@
 		<header class="menu">
 			<nav>
 				<ul>
-					<li><a href="/"><i class="fa-solid fa-house"></i></a></li>
+					<li><a href="/contact"><i class="fas fa-envelope"></i></a></li>
+					<li><a href="https://api.whatsapp.com/send?phone=+520123456789" target="_blank"><i class="fab fa-whatsapp"></i></a></li>
+					<li><a href="https://facebook.com/" target="_blank"><i class="fab fa-facebook-messenger"></i></a></li>
 					<li><a href="https://facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a></li>
 					<li><a href="https://instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a></li>
 					<li><a href="tel:+520123456789" target="_blank"><i class="fas fa-phone"></i></a></li>
-					<li><a href="https://api.whatsapp.com/send?phone=+520123456789" target="_blank"><i class="fab fa-whatsapp"></i></a></li>
-					<li><a href="https://facebook.com/" target="_blank"><i class="fab fa-facebook-messenger"></i></a></li>
-					<li><a href="/contact"><i class="fas fa-envelope"></i></a></li>
 				</ul>
 			</nav>
 			<nav>
 				<ul>
+					<li><a href="/"><i class="fa-solid fa-house"></i></a></li>
+					<li><a href="/products"><i class="fa-solid fa-list-ul"></i>	</a></li>
 					<li><a href="/cart"><i class="fas fa-cart-plus"></i>3 {$lang.products}</a></li>
 					<li><a href="/cart"><i class="fas fa-credit-card"></i>$ 1,500.00</a></li>
 					<li><a href="/cart"><i class="fas fa-dollar-sign"></i>MXN</a></li>
